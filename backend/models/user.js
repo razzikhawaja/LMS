15360e2156e51ba3072fe5638e0dbc354fbe84ca
Razzi Khawaja <i201757@nu.edu.pk>
2024-06-05 12:46:02 +0500

    implemented models

---
+++ backend/models/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, name, email, contact_number) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.contact_number = contact_number;
+    }
+}
+
+module.exports = User;
